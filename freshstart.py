607ecae5a216df8fde94cf1083e87440ded69bbb
unknown <greatabhi.abhishek@gmail.com>
2019-08-14 05:30:10 +0530

    version control for python project v Please enter the commit message for your changes. Lines starting

---
+++ freshstart.py
@@ -0,0 +1,29 @@
+x=3
+
+def function1():
+    global x
+    x=5
+    print (x)
+
+def function2():
+    str1 = "My! name is @Abhishek"
+    str2= str1.split(" ")
+    trimmedlist = [x.strip("!, @") for x in str2]
+    print(trimmedlist)
+    
+def function3():
+    Days = {"Mon", "Tue"} 
+    for i,d in enumerate(Days):
+        print (str(i) + " " + "day of the week is" + " " +str(d))
+
+class myclass():
+    def method1(self):
+        function1()
+
+
+if __name__=="__main__":
+    function1()
+    function2()
+    function3()
+    s1=myclass()
+    s1.method1()
