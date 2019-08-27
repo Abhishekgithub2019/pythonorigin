25b13e9a826c40f6d318b9114a97bed86d07567a
unknown <greatabhi.abhishek@gmail.com>
2019-08-28 03:58:14 +0530

    Final change

--- refresherprogram.py
+++ refresherprogram.py
@@ -4,10 +4,19 @@ def function1():
     SPLITTEDVAR= [x.strip("!, @") for x in TRIMMEDVAR] 
     print (SPLITTEDVAR)
 
+def function2():
+    DAYS = ["Mon", "TUE"]
+    for i,d in enumerate (DAYS):
+        print (str(i) + " " + "Day of the week is" + " " + str(d))
+        
 class myclass():
     def method1 (self):
         function1()
+    def method2(self):
+        function2()
+
 
 if __name__ == "__main__":
     s1=myclass()
     s1.method1()
+    s1.method2()
