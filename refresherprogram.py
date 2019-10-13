09f74de903e99e88d52e20e4a64fabb3e37bdf58
unknown <greatabhi.abhishek@gmail.com>
2019-10-14 04:42:56 +0530

    After a while

--- refresherprogram.py
+++ refresherprogram.py
@@ -1,20 +1,18 @@
 def function1():
-    VAR1="My name! is Singh$"
-    VAR2= VAR1.split(" ")
-    VAR3=[x.strip("!, $") for x in VAR2]
-    print (VAR3)
+    VAR1="My name! is Abhishek"
+    SPLITTEDVAR=VAR1.split(" ")
+    TRIMMEDVAR=[x.strip("!") for x in SPLITTEDVAR]
+    print (TRIMMEDVAR)
 
 def function2():
-    VAR4={"Mon", "Tue"}
-    for i,d in enumerate(VAR4):
-        print ("First day is" + " " + d)
-
+    VAR1 = ["Mon", "Tue", "Wed"]
+    for i,d in enumerate(VAR1):
+        print (str(i) + " " + "day of the week is" + " " + str(d))
 
 class myclass():
     def method1(self):
         function1()
         function2()
-
 
 if __name__=="__main__":
     s1=myclass()
