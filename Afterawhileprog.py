f993ce5756668724319befdc505c1ead36d6b536
unknown <greatabhi.abhishek@gmail.com>
2020-01-29 06:25:52 +0530

    modified content as per new skills

--- Afterawhileprog.py
+++ Afterawhileprog.py
@@ -1,23 +1,55 @@
+
+def function5():
+    vowels = {"a","e", "i", "o", "u"}
+    STR1 = "tata consultancy services"
+    Count = 0
+    i=0
+    while i < len(STR1):
+        if STR1[i] in vowels:
+            Count = Count + 1
+        i=i+1 
+    print (Count)
+
 def function1():
-    print ("Hello World")
+    STR1 = "KOKOMELON MEINE BRUDER!"
+    SPLITTEDVAR = STR1.split(" ")
+    TRIMMEDVAR = [x.strip("!") for x in SPLITTEDVAR]
+    print (TRIMMEDVAR)
 
 def function2():
-    STR1 = ["Mon", "Tue"]
-    for i,d in enumerate (STR1):
-        print (str(i) + " " + "day of the week is" + " " + str(d))
+    DAYS = ["Mon", "TUE"]
+    DAYS.append("Wed")
+    for i,d in enumerate (DAYS):
+        if type(DAYS) != tuple:
+            print (str(i)+ " "+ "of the week is" + str(d))
+    print(DAYS)
+    del DAYS[2]
+    print (DAYS)
 
 def function3():
-    STR1 = ("Abhishek IS FINICKY!")
-    SPLIT1 = STR1.split(" ")
-    TRIM1 = [x.strip ("!") for x in SPLIT1]
-    print (TRIM1)
+    STR1 = bytearray ("abc", encoding="UTF-8")
+    STR1[0] = 65
+    if STR1.startswith (b"A"):
+        print (STR1)
+
+def function4():
+    DICT1 = {1:"Abhi", 2:"Kanu",3:"Kokomelon"}
+    DICT2 = {4:"Ankuri"}
+    DICT1.update(DICT2)
+    print (DICT1)
+
 
 class myclass():
     def method1(self):
         function1()
         function2()
-        function3()   
+    def method2(self):
+        function3()
+        function5()
 
-if __name__ == "__main__":
+if __name__=="__main__":
     s1=myclass()
     s1.method1()
+    s1.method2()
+    function4()
+
---
+++ UserInput.py
@@ -0,0 +1,9 @@
+import sys
+
+def function1():
+    x = (sys.argv[[1]])
+    print(x)
+
+
+if __name__ == "__main__":
+    function1()
